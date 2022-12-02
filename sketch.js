8c45e2a0b7469967e8cf9a862914c2b67c2bb38f
seojeong01 <102014750+seojeong01@users.noreply.github.com>
2022-12-02 16:01:25 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
 var wave;
 var playing = false;
 var button;
+let r,g,b;
 
 function setup() {
-  createCanvas(720, 256);
+  createCanvas(720, 720);
+  r = random(50, 255);
+  g = random(0, 200);
+  b = random(50,255);
+
   wave = new p5.Oscillator();
   wave.setType('sine');
   //wave.start();
@@ -14,7 +19,9 @@ function setup() {
   button.mousePressed(toggle);
 }
 function draw() {
-  //background(220);
+  background(r,g,b);
+  //console.log('draw')
+  Text(accelerationX, accelerationY, accelerationZ,10,10);
 }
 function toggle() {
   if(!playing){
@@ -25,4 +32,8 @@ function toggle() {
     wave.amp(0,1);
     playing = false;
   }
+  r = map(accelerationX, -90, 90, 100, 175);
+  g = map(accelerationY, -90, 90, 100, 200);
+  b = map(accelerationZ, -90, 90, 100, 200);
+
 }
